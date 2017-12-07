4aa4d8825ae1362adf431d6b09681d813a1325ca
Krister Jansson <krustur@gmail.com>
2017-12-07 23:10:02 +0100

    Releasify now applies semantic versioning to assemblies

---
+++ ApplySemanticVersioningToAssemblies.ps1
@@ -0,0 +1,98 @@
+Param(
+  [string]$pathToSearch = $env:BUILD_SOURCESDIRECTORY,
+  [string]$buildNumber = $env:BUILD_BUILDNUMBER,
+  [regex]$pattern = "\d+\.\d+\.\d+\.\d+",
+  [string]$makeReleaseVersion = [bool]::FalseString,
+  [string]$preReleaseName = "",
+  [string]$includeRevInPreRelease = [bool]::FalseString,
+  [string]$patternSplitCharacters = ".",
+  [string]$versionNumbersInAssemblyVersion = "2"
+)
+
+Write-Output $makeReleaseVersion
+$ErrorActionPreference = "Stop"
+
+if ([bool]::Parse($makeReleaseVersion)) {
+    $preReleaseName = $null
+    $includeRevInPreRelease = [bool]::FalseString
+}
+elseif ([string]::IsNullOrEmpty($preReleaseName)) {
+    Write-Host "Prerelease name is not set"
+    exit 1
+}
+
+$searchFilter = "AssemblyInfo.*"
+if ($buildNumber -match $pattern -ne $true) {
+    Write-Host "Could not extract a version from [$buildNumber] using pattern [$pattern]"
+    exit 2
+}
+
+# Declare functions
+
+function Replace-Version($content, $version, $attribute, $language) {
+    if ($language -eq "cs") {
+        $versionAttribute = "[assembly: $attribute(""$version"")]"
+        $pattern = "\[assembly: $attribute\("".*""\)\]"
+    }
+    elseif ($language -eq "vb") {
+        $versionAttribute = "<Assembly: $attribute(""$version"")>"
+        $pattern = "\<Assembly: $attribute\("".*""\)\>"
+    }
+
+    $versionReplaced = $false
+
+    $content = $content | %{
+        if ($_ -match $pattern) {
+            $versionReplaced = $true
+            Write-Host "     * Replaced $($Matches[0]) with $versionAttribute"
+            $_ = $_ -replace [regex]::Escape($Matches[0]),$versionAttribute
+        }
+        $_
+    }
+	
+    if (-not $versionReplaced) {
+        Write-Host "     * Added $versionAttribute to end of content"
+        $content += [System.Environment]::NewLine + $versionAttribute
+    }
+    return $content
+}
+
+function Get-VersionString($numberOfVersions, $extractedBuildNumbers) {
+    return [string]::Join(".",($extractedBuildNumbers | select -First ([int]::Parse($numberOfVersions))))
+}
+
+# Set version variables
+$extractedBuildNumbers = @($Matches[0].Split(([char[]]$patternSplitCharacters)))
+if ($extractedBuildNumbers.Length -ne 4) {
+    Write-Host "The extracted build number $($Matches[0]) does not contain the expected 4 elements"
+    exit 2
+}
+$version = Get-VersionString -numberOfVersions $versionNumbersInAssemblyVersion -extractedBuildNumbers $extractedBuildNumbers
+$fileVersion = Get-VersionString -numberOfVersions "4" -extractedBuildNumbers $extractedBuildNumbers
+$informationalVersion = "$($extractedBuildNumbers[0]).$($extractedBuildNumbers[1]).$($extractedBuildNumbers[2])"
+if ([string]::IsNullOrEmpty($preReleaseName) -ne $true) {
+    $informationalVersion += "-$preReleaseName"
+    if ([bool]::Parse($includeRevInPreRelease)) {
+        $informationalVersion += ([int]$extractedBuildNumbers[3]).ToString("0000")
+    }
+}
+Write-Host "Using version $version, file version $fileVersion and informational version $informationalVersion"
+
+gci -Path $pathToSearch -Filter $searchFilter -Recurse | %{
+    Write-Host "  -> Changing $($_.FullName)"
+         
+    # remove the read-only bit on the file
+    sp $_.FullName IsReadOnly $false
+ 
+    # language
+    $language = $_.Extension.ToLower().Substring(1,2)
+    Write-Host "  -> Language $($language)"
+    
+    # run the regex replace
+    $content = gc $_.FullName
+    $content = Replace-Version -content $content -version $version -attribute 'AssemblyVersion' -language $language
+    $content = Replace-Version -content $content -version $fileVersion -attribute 'AssemblyFileVersion' -language $language
+    $content = Replace-Version -content $content -version $informationalVersion -attribute 'AssemblyInformationalVersion' -language $language
+    $content | sc $_.FullName -Encoding UTF8
+}
+Write-Host "Done!"
--- releasify.ps1
+++ releasify.ps1
@@ -1,4 +1,14 @@
+Param(
+  [Parameter(Mandatory=$true)][int]$major,
+  [Parameter(Mandatory=$true)][int]$minor,
+  [Parameter(Mandatory=$false)][bool]$makeReleaseVersion = $True
+)
 
+$ErrorActionPreference = "Stop"
+
+##
+## Build
+##
 $msbuildpath = & "$(${env:ProgramFiles(x86)})\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
 if ($msbuildpath) {
   $msbuildpath = join-path $msbuildpath 'MSBuild\15.0\Bin\MSBuild.exe'
@@ -11,3 +21,29 @@ if ($msbuildpath) {
 if ($LastExitCode -ne 0) {
     Write-Error "Build failed!!!"
 }
+
+##
+## Apply semantic versioning
+##
+$date = Get-Date
+$year = $date.Year.ToString().Substring(2)
+$dayofyear = $date.DayOfYear.ToString()
+$rev = [math]::Round($date.TimeOfDay.TotalSeconds/4)
+$patch = "$year$dayofyear"
+$buildNumber = "$major.$minor.$patch.$rev"
+Write-Output $buildNumber
+#Write-Output [bool]::FalseString
+
+$applySemVerExpression = ".\ApplySemanticVersioningToAssemblies.ps1 -pathToSearch '.' -buildNumber $buildNumber -makeReleaseVersion $makeReleaseVersion -preReleaseName 'beta' -includeRevInPreRelease $([bool]::FalseString) -versionNumbersInAssemblyVersion '4'"
+Write-Output $applySemVerExpression
+Invoke-Expression $applySemVerExpression
+  <#
+  [string]$pathToSearch = $env:BUILD_SOURCESDIRECTORY,
+  [string]$buildNumber = $env:BUILD_BUILDNUMBER,
+  [regex]$pattern = "\d+\.\d+\.\d+\.\d+",
+  [string]$makeReleaseVersion = [bool]::FalseString,
+  [string]$preReleaseName = "",
+  [string]$includeRevInPreRelease = [bool]::FalseString,
+  [string]$patternSplitCharacters = ".",
+  [string]$versionNumbersInAssemblyVersion = "2"
+  #>
